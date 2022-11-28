486489c1f4ea622f5a2f306ee9a84db7738baa3f
Luis-Felipe-N <luisjefelipe09@gmail.com>
2022-11-28 19:46:48 -0300

    adicionando campo proximo

--- Hash.js
+++ Hash.js
@@ -20,7 +20,7 @@ class Hash {
   
     /**
      * Função responsável por inserir um valor na tabela tratando as colisões.
-     * - Se não tiver um elemento no mesmo index da tabela, apenas adiciona.
+     * - Se não tiver elemento na lista de encadeamento, apenas adiciona.
      * - Se tiver algum elemento, verifica se é diferente, se não for adiciona.
      */
     inserir(valor) {
@@ -28,17 +28,19 @@ class Hash {
       const bucket = this.tabela[index];
   
       if (!bucket) {
-        this.tabela[index] = [valor];
+        this.tabela[index] = [{valor: valor, prox: null}];
       } else {
-        const temMesmoValor = bucket.find((item) => item === valor);
+        const temMesmoValor = bucket.find((item) => item.valor === valor);
         if (!temMesmoValor) {
-          bucket.push(valor);
+          const ultimoElemento = bucket[0]
+          const elemento = {valor: valor, prox: ultimoElemento.valor}
+          bucket.unshift(elemento);
         }
       }
     }
   
     /**
-     * Função responsável por buscar o elemento na tabela hash.
+     * Função responsável por buscar o elemento da tabela hash.
      * - Pelo valor é possível gerar um hash para encontrar o index da lista de encadeamento.
      * - Depois procura na lista de encadeamento o valor.
      */
@@ -47,7 +49,7 @@ class Hash {
       const bucket = this.tabela[index];
       
       if (bucket) {
-        const temMesmaChave = bucket.find((item) => item === valor);
+        const temMesmaChave = bucket.find((item) => item.valor === valor);
         
         if (temMesmaChave) {
           return temMesmaChave;
@@ -56,18 +58,29 @@ class Hash {
       return undefined;
     }
   
+    /**
+     * Função responsável por remover o elemento da tabela hash.
+     * - Encontra a lista de encadeamento e procura o elemento dentro da lista retornando o index caso encontre
+     * -  Se tiver um antecessor do elemento, o prox é passado para ele
+     */
     remover(valor) {
       let index = this.hash(valor);
       const bucket = this.tabela[index];
+
       if (bucket) {
-        const temMesmoItem = bucket.find((item) => item === valor);
-        if (temMesmoItem) {
-          bucket.splice(bucket.indexOf(temMesmoItem), 1);
+        const temOElemento = bucket.findIndex((item) => item.valor === valor);
+        if (temOElemento >= 0) {
+          if (temOElemento > 0) {
+            bucket[temOElemento - 1].prox = bucket[temOElemento].prox
+          }
+          bucket.splice(temOElemento, 1);
         }
       }
     }
   
     display() {
+      console.clear()
+      console.table(this.tabela)
       for (let i = 0; i < this.tabela.length; i++) {
         if (this.tabela[i]) {
           console.log(i, this.tabela[i]);
@@ -139,7 +152,7 @@ class Hash {
               message: 'Qual número deseja inserir?'
             });
           
-            console.log(this.busca(valorBusca))
+            console.log('Valor encontrado', this.busca(valorBusca))
             break;
           case 3:
             // Impressão
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # tabela-hash
 Trablado de Estrutura de dados 2 
 
+## Como rodar?
+
+```
+# instale o node
+
+# Depois siga os comando abaixo:
+npm i
+
+node Hash.js
+```
+
 ### Funcionando no CLI
-[Video mostrando o funcionamento do programa](public/assets/example.gif)
+![Video mostrando o funcionamento do programa](public/assets/example.gif)
